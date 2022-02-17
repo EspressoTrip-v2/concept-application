9c2f515d72fb2a5cbd3a744c6e9441b34603c3e5
Juan Botha <juan.botha@wyzetalk.com>
2022-02-17 12:12:28 +0200

    Wait for condition Ready

--- pod-deploys.sh
+++ pod-deploys.sh
@@ -2,13 +2,6 @@
 
 # This will deploy a single operator for Postgres as the codebase requires ssl so single pod is too much work
 # everything else will be a standalone pod
-# Postgres Operators
-kubectl apply -f infra/infra-operators/operators/postgres/configmap.yaml
-kubectl apply -f infra/infra-operators/operators/postgres/operator-service-account-rbac.yaml
-kubectl apply -f infra/infra-operators/operators/postgres/postgres-operator.yaml
-
-# Postgres cluster deployments
-kubectl apply -f infra/infra-operators/operator-deployments/postgres/analytic-postgres-cluster.yaml
 
 # MongoDB Deployments
 kubectl apply -f infra/infra-pods/auth-mongo.yaml
@@ -18,3 +11,14 @@ kubectl apply -f infra/infra-pods/task-mongo.yaml
 
 # RabbitMQ Deployments
 kubectl apply -f infra/infra-pods/rabbitmq.yaml
+
+# Postgres Operators
+kubectl apply -f infra/infra-operators/operators/postgres/configmap.yaml
+kubectl apply -f infra/infra-operators/operators/postgres/operator-service-account-rbac.yaml
+kubectl apply -f infra/infra-operators/operators/postgres/postgres-operator.yaml
+
+# Wait for deployments to be ready
+kubectl wait --for=condition=Ready --all deployments
+
+# Postgres cluster deployments
+kubectl apply -f infra/infra-operators/operator-deployments/postgres/analytic-postgres-cluster.yaml
