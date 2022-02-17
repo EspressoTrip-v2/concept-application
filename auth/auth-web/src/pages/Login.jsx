87dc702ac0086a25d709b9f17c7f2c5850045472
Ruben Verster <rrverster31415@gmail.com>
2022-02-17 11:38:50 +0200

    removed type for signup

--- auth/auth-web/src/pages/Login.jsx
+++ auth/auth-web/src/pages/Login.jsx
@@ -28,7 +28,6 @@ const Login = () => {
     const user = {
       email: input.email,
       password: input.password,
-      type: signUp,
     };
     await axios.post('/api/auth/local', user);
     navigate('/');
