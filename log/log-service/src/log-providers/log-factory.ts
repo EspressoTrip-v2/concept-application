57715069d0ed1bb23bf2917bb274dab47e022bb3
Juan Botha <juan.botha@wyzetalk.com>
2022-02-21 00:43:18 +0200

    Logging factories and logging stuff added

--- log/log-service/src/log-providers/log-factory.ts
+++ log/log-service/src/log-providers/log-factory.ts
@@ -8,8 +8,13 @@ export class LogFactory {
     static getLogger(logType: LogProviderTypes): LogClass {
         switch (logType) {
             case LogProviderTypes.loggly:
+                if (!process.env.LOGGLY_TOKEN) throw new Error("LOGGLY_TOKEN must be defined");
+                if (!process.env.LOGGLY_SUBDOMAIN) throw new Error("LOGGLY_SUBDOMAIN must be defined");
                 return new LogglyProvider();
+
             case LogProviderTypes.papertrail:
+                if (!process.env.PAPER_TRAIL_HOST) throw new Error("PAPER_TRAIL_HOST must be defined");
+                if (!process.env.PAPER_TRAIL_PORT) throw new Error("PAPER_TRAIL_PORT must be defined");
                 return new PapertrailProvider();
             default:
                 throw new Error("No matching log provider found.");
