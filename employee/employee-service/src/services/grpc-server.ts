e6986d1af3d9ef81d971f86cc611a7429d2f0c14
Juan Botha <juan.botha@wyzetalk.com>
2022-02-23 14:27:23 +0200

    gRPC server employee updated

--- employee/employee-service/src/services/grpc-server.ts
+++ employee/employee-service/src/services/grpc-server.ts
@@ -1,12 +1,12 @@
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
-import { AbstractGrpcServer, EmployeeMsg, LogCodes, LogPublisher, MicroServiceNames, rabbitClient } from "@espressotrip-org/concept-common";
+import { AbstractGrpcServer, LogCodes, LogPublisher, MicroServiceNames } from "@espressotrip-org/concept-common";
 import amqp from "amqplib";
 import { ProtoGrpcType } from "./proto/employee";
 import { EmployeeServiceHandlers } from "./proto/employeePackage/EmployeeService";
 import { EmployeeId } from "./proto/employeePackage/EmployeeId";
 import { GrpcEmployeeAttributes } from "./proto/employeePackage/GrpcEmployeeAttributes";
-import { Employee, EmployeeDoc } from "../models";
+import { Employee } from "../models";
 import { GrpcResponsePayload } from "./proto/employeePackage/GrpcResponsePayload";
 import { DeleteEmployeePublisher } from "../events";
 
@@ -53,26 +53,84 @@ export class GrpcServer extends AbstractGrpcServer {
                 }
             });
             this.m_logger.publish(LogCodes.CREATED, "Employee created", "CreateEmployee()", `email: ${employee.email}, id: ${employee.id}`);
+            callback(null, {
+                status: 200,
+                data: employee,
+            });
         },
+
         DeleteEmployee: async (call: grpc.ServerUnaryCall<EmployeeId, GrpcResponsePayload>, callback: grpc.sendUnaryData<GrpcResponsePayload>) => {
             const { id }: EmployeeId = call.request;
             const deletedEmployee = await Employee.findByIdAndDelete(id);
-            if (deletedEmployee) {
-                this.m_logger.publish(
-                    LogCodes.DELETED,
-                    "Employee deleted successfully",
-                    "DeleteEmployee()",
-                    `email: ${deletedEmployee.email}, id: ${deletedEmployee.id}`
-                );
-                new DeleteEmployeePublisher(this.m_rabbitConnection!).publish(Employee.convertToGrpcMessage(deletedEmployee));
-                return callback(null, {
-                    status: 200,
-                    data: deletedEmployee,
+            if (!deletedEmployee) {
+                this.m_logger.publish(LogCodes.ERROR, "Employee not found", "DeleteEmployee()", `Employee id: ${id} does not exist`);
+                return callback({
+                    code: grpc.status.NOT_FOUND,
+                    details: "Employee not found.",
                 });
             }
+            this.m_logger.publish(
+                LogCodes.DELETED,
+                "Employee deleted successfully",
+                "DeleteEmployee()",
+                `email: ${deletedEmployee.email}, id: ${deletedEmployee.id}`
+            );
+            new DeleteEmployeePublisher(this.m_rabbitConnection!).publish(Employee.convertToGrpcMessage(deletedEmployee));
+            return callback(null, {
+                status: 200,
+                data: deletedEmployee,
+            });
         },
-        GetEmployee: (call: grpc.ServerUnaryCall<EmployeeId, GrpcResponsePayload>, callback: grpc.sendUnaryData<GrpcResponsePayload>) => {},
-        UpdateEmployee: (call: grpc.ServerUnaryCall<GrpcEmployeeAttributes, GrpcResponsePayload>, callback: grpc.sendUnaryData<GrpcResponsePayload>) => {},
+
+        GetEmployee: async (call: grpc.ServerUnaryCall<EmployeeId, GrpcResponsePayload>, callback: grpc.sendUnaryData<GrpcResponsePayload>) => {
+            const { id }: EmployeeId = call.request;
+            const employee = await Employee.findById(id);
+            if (!employee) {
+                this.m_logger.publish(LogCodes.ERROR, "Employee not found", "DeleteEmployee()", `Employee id: ${id} does not exist`);
+                return callback({
+                    code: grpc.status.NOT_FOUND,
+                    details: "Employee not found.",
+                });
+            }
+            return callback(null, {
+                status: 200,
+                data: employee,
+            });
+        },
+        UpdateEmployee: async (call: grpc.ServerUnaryCall<GrpcEmployeeAttributes, GrpcResponsePayload>, callback: grpc.sendUnaryData<GrpcResponsePayload>) => {
+            const employeeUpdate: GrpcEmployeeAttributes = call.request;
+            const { id } = employeeUpdate;
+            delete employeeUpdate.id;
+
+            const employee = await Employee.findById(id);
+            if (!employee) {
+                this.m_logger.publish(LogCodes.ERROR, "Employee not found", "UpdateEmployee()", `email: ${employeeUpdate.email}, id: ${id}`);
+                return callback({
+                    code: grpc.status.NOT_FOUND,
+                    details: "Employee not found",
+                });
+            }
+            employee.set({ ...employeeUpdate });
+            employee.save().catch(error => {
+                if (error) {
+                    this.m_logger.publish(
+                        LogCodes.ERROR,
+                        "Could not update employee, employee save failed",
+                        "LoginGitHubUser",
+                        `email: ${employee.email}, id: ${employee.id}`
+                    );
+                    return callback({
+                        code: grpc.status.INTERNAL,
+                        details: "Could not update employee, employee save failed",
+                    });
+                }
+            });
+            this.m_logger.publish(LogCodes.ERROR, "Employee updated", "UpdateEmployee()", `email: ${employee.email}, id: ${employee.id}`);
+            return callback(null, {
+                status: 200,
+                data: employee,
+            });
+        },
     };
 
     constructor(rabbitConnection: amqp.Connection) {
