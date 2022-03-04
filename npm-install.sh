093e4c0e498a23cbd7fe74d34279cf27ab17376c
Juan Botha <juan.botha@wyzetalk.com>
2022-03-04 12:06:09 +0200

    Fixed package-lock.json and install scripts

--- npm-install.sh
+++ npm-install.sh
@@ -6,10 +6,11 @@
 cd auth/auth-api
 echo "[auth-api]: Installing"
 npm i --silent
+echo "[auth-api]: Done"
 cd ../auth-service
 echo "[auth-service]: Installing"
 npm i --silent
-
+echo "[auth-service]: Done"
 # Root
 cd ../../
 
@@ -17,10 +18,11 @@ cd ../../
 cd analytic/analytic-api
 echo  "[analytic-api]: Installing"
 npm i --silent
+echo  "[analytic-api]: Done"
 cd ../analytic-service
 echo "[analytic-service]: Installing"
 npm i --silent
-
+echo "[analytic-service]: Done"
 # Root
 cd ../../
 
@@ -28,16 +30,25 @@ cd ../../
 cd employee/employee-api
 echo "[employee-api]: Installing"
 npm i --silent
+echo "[employee-api]: Done"
+
 cd ../employee-service
 echo "[employee-service]: Installing"
 npm i --silent
-
+echo "[employee-service]: Done"
 cd ../../
 
 # Log
 cd log/log-service
 echo "[log-service]: Installing"
 npm i --silent
+echo "[log-service]: Done"
 
+cd ../../
 
+# WEb-UI
+cd web-ui
+echo "[web-ui]: Installing"
+npm i --silent
+echo "[web-ui]: Done"
 cd ../../
--- npm-update-common.sh
+++ npm-update-common.sh
@@ -5,39 +5,42 @@
 # Auth
 cd auth/auth-api
 echo "[auth-api]: Updating"
-npm update @espressotrip-org/concept-common
+npm update @espressotrip-org/concept-common --silent
+echo "[auth-api]: Done"
 cd ../auth-service
 echo "[auth-service]: Updating"
-npm update @espressotrip-org/concept-common
-
+npm update @espressotrip-org/concept-common --silent
+echo "[auth-service]: Done"
 # Root
 cd ../../
 
 # Analytic
 cd analytic/analytic-api
 echo  "[analytic-api]: Updating"
-npm update @espressotrip-org/concept-common
+npm update @espressotrip-org/concept-common --silent
+echo  "[analytic-api]: Done"
 cd ../analytic-service
 echo "[analytic-service]: Updating"
-npm update @espressotrip-org/concept-common
-
+npm update @espressotrip-org/concept-common --silent
+echo "[analytic-service]: Done"
 # Root
 cd ../../
 
 # Employee
 cd employee/employee-api
 echo "[employee-api]: Updating"
-npm update
+npm update @espressotrip-org/concept-common --silent
+echo "[employee-api]: Done"
 cd ../employee-service
 echo "[employee-service]: Updating"
-npm update
-
+npm update @espressotrip-org/concept-common --silent
+echo "[employee-service]: Done"
 cd ../../
 
 # Log
 cd log/log-service
 echo "[log-service]: Updating"
-npm update @espressotrip-org/concept-common
-
+npm update @espressotrip-org/concept-common --silent
+echo "[log-service]: Done"
 
 cd ../../
