37c309a073c873e0a611c45c4509f5946cfbbb75
Ruben Verster <rrverster31415@gmail.com>
2022-03-05 20:11:32 +0200

    implemented redirect templates, updated logos

--- auth/auth-api/src/routes/google-auth.ts
+++ auth/auth-api/src/routes/google-auth.ts
@@ -1,35 +1,30 @@
-import express, { Request, Response } from 'express';
-import { LogCodes } from '@espressotrip-org/concept-common';
-import { userGrpcClient } from '../services';
-import { GoogleGrpcUser } from '../services/proto/userPackage/GoogleGrpcUser';
-import { LocalLogger } from '../utils';
+import express, { Request, Response } from "express";
+import { LogCodes } from "@espressotrip-org/concept-common";
+import { userGrpcClient } from "../services";
+import { GoogleGrpcUser } from "../services/proto/userPackage/GoogleGrpcUser";
+import { LocalLogger } from "../utils";
 
 const router = express.Router();
 
-router.get('/api/auth/google/redirect', async (req: Request, res: Response) => {
-  try {
-    const googleUser: GoogleGrpcUser = req.session?.grant.response.profile;
+router.get("/api/auth/google/redirect", async (req: Request, res: Response) => {
+    try {
+        const googleUser: GoogleGrpcUser = req.session?.grant.response.profile;
 
-    /** Make the request to the gRPC auth-service server */
-    const rpcResponse = await userGrpcClient.loginGoogleUser(googleUser);
+        /** Make the request to the gRPC auth-service server */
+        const rpcResponse = await userGrpcClient.loginGoogleUser(googleUser);
 
-    /** Log Event */
-    LocalLogger.log(
-      LogCodes.INFO,
-      `Google SignIn`,
-      '/api/auth/google/redirect',
-      `email: ${googleUser.email}`
-    );
+        /** Log Event */
+        LocalLogger.log(LogCodes.INFO, `Google SignIn`, "/api/auth/google/redirect", `email: ${googleUser.email}`);
 
-    /** Add to the session */
-    req.session = {
-      payload: rpcResponse.jwt,
-    };
+        /** Add to the session */
+        req.session = {
+            payload: rpcResponse.jwt,
+        };
 
-    res.render('redirect');
-  } catch (error) {
-    res.render('error');
-  }
+        res.render("redirect");
+    } catch (error) {
+        res.render("error");
+    }
 });
 
 export { router as googleAuthRouter };
