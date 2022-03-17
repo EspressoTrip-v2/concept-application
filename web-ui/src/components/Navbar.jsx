227038022fda56061ab579f5e6785d795c6ac4e0
Ruben Verster <rrverster31415@gmail.com>
2022-03-17 12:59:43 +0200

    fe overload

--- web-ui/src/components/Navbar.jsx
+++ web-ui/src/components/Navbar.jsx
@@ -4,10 +4,14 @@ import { useStoreState, useStoreActions } from "easy-peasy";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useState } from "react";
+
+import logo from "../img/acme-pill.svg";
+
 const Navbar = () => {
     const navigate = useNavigate();
     const userAuthorised = useStoreState(state => state.userAuthorised);
     const setUserAuthorised = useStoreActions(actions => actions.setUserAuthorised);
+    const setUser = useStoreActions(actions => actions.setUser);
 
     const [loading, setLoading] = useState(false);
 
@@ -16,7 +20,8 @@ const Navbar = () => {
         const logout = await axios.post("/api/auth/signout");
         if (logout.status === 200) {
             await setUserAuthorised(false);
-            await navigate("/login");
+            await setUser({});
+            await navigate("/");
         }
         if (logout.status !== 200) {
             toast.error("Error Logging Out. Please Try Again", {
@@ -32,41 +37,32 @@ const Navbar = () => {
         setLoading(false);
     };
     return (
-        <div className="w-100 conatiner">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between align-items-center">
-                <div className="container-fluid ">
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div className="collapse navbar-collapse w-100" id="navbarSupportedContent">
-                        <ul className="navbar-nav w-100 me-100 mb-2 mb-lg-0 d-flex justify-content-between align-items-center">
-                            <li className="nav-item">
-                                <Link to="/">
-                                    <img src="./assets/acme-pill.svg" width="111px" height="55px" alt="Logo" />
-                                </Link>
-                            </li>
-
-                            <li>
-                                {userAuthorised && (
-                                    <button className="btn btn-dark" disabled={loading} onClick={signOut}>
-                                        Sign Out
-                                    </button>
-                                )}
-                            </li>
-                        </ul>
-                    </div>
+        <div className="navigation">
+            <nav className="navigation_desktop">
+                <div className="nav_left">
+                    <Link to="/">
+                        <img src={logo} className="navigation_logo" width="111px" height="55px" alt="ACME" />
+                    </Link>
+                </div>
+                <div className="nav">
+                    {userAuthorised && (
+                        <button className="btn btn-dark" disabled={loading} onClick={signOut}>
+                            Sign Out
+                        </button>
+                    )}
+                </div>
+            </nav>
+            <nav className="navigation_mobile">
+                <div className="nav_left">
+                    <Link to="/">
+                        <img src={logo} width="111px" height="55px" alt="ACME" />
+                    </Link>
                 </div>
             </nav>
         </div>
     );
 };
 
+{
+}
 export default Navbar;
