b3b300f32d09ddaa0e361ca555a39cdd2c6d07f2
Juan Botha <juan.botha@wyzetalk.com>
2022-03-29 23:02:58 +0200

    Added Google and Github login employee dash

--- employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go
+++ employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go
@@ -15,6 +15,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"strconv"
 )
 
 type EmployeeDashRpcHandlers struct {
@@ -31,28 +32,29 @@ func (r *EmployeeDashRpcHandlers) LoginGoogleUser(ctx context.Context, user *emp
 	var emp models.Employee
 	filter := bson.D{{"email", user.GetEmail()}}
 	err := r.mongo.FindOneEmployee(ctx, filter, &emp)
-	ok := r.onFailure(err, logcodes.ERROR, "Employee not registered", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:31")
+	ok := r.onFailure(err, logcodes.ERROR, "Employee not registered", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:35")
 	if !ok {
 		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("Employee not registered: %v", err.Error()))
 	}
+
+	if (emp.SignInType != string(oath.GOOGLE) || user.GetSub() != emp.ProviderId) && emp.SignInType != string(oath.UNKNOWN) {
+		err := errors.New("Invalid credentials")
+		r.onFailure(err, logcodes.ERROR, "Invalid Google credentials", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:42")
+		return nil, status.Errorf(codes.PermissionDenied, err.Error())
+	}
+
+	if emp.SignInType == string(oath.GITHUB) || emp.SignInType == string(oath.LOCAL) {
+		err := errors.New("Not Authorized")
+		r.onFailure(err, logcodes.ERROR, "Not Authorized", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:48")
+		return nil, status.Errorf(codes.PermissionDenied, err.Error())
+	}
+
 	if emp.SignInType == string(oath.UNKNOWN) {
 		rs, customError := GrpcClient().LoginGoogleUser(ctx, (*userPackage.GoogleGrpcUser)(user))
 		if customError != nil {
 			err := errors.New(customError.Message[0])
-			r.onFailure(err, logcodes.ERROR, customError.Message[0], "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:55")
+			r.onFailure(err, logcodes.ERROR, customError.Message[0], "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:56")
 			return nil, status.Errorf(codes.Unknown, customError.Message[0])
-		}
-
-		if emp.SignInType != string(oath.GOOGLE) || user.GetSub() != emp.ProviderId {
-			err := errors.New("Invalid credentials")
-			r.onFailure(err, logcodes.ERROR, "Invalid Google credentials", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:41")
-			return nil, status.Errorf(codes.PermissionDenied, err.Error())
-		}
-
-		if emp.SignInType == string(oath.GITHUB) || emp.SignInType == string(oath.LOCAL) {
-			err := errors.New("Not Authorized")
-			r.onFailure(err, logcodes.ERROR, "Not Authorized", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:47")
-			return nil, status.Errorf(codes.PermissionDenied, err.Error())
 		}
 
 		response := employeePackage.GrpcResponsePayload{
@@ -65,7 +67,7 @@ func (r *EmployeeDashRpcHandlers) LoginGoogleUser(ctx context.Context, user *emp
 
 	// Encode the JWT
 	jwt, err := utils.EncodeJwt(&emp)
-	ok = r.onFailure(err, logcodes.ERROR, "Unable to encode JWT", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:68")
+	ok = r.onFailure(err, logcodes.ERROR, "Unable to encode JWT", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:70")
 	if !ok {
 		return nil, status.Errorf(codes.Internal, fmt.Sprintf("Unable to encode JWT payload"))
 	}
@@ -102,13 +104,148 @@ func (r *EmployeeDashRpcHandlers) LoginGoogleUser(ctx context.Context, user *emp
 }
 
 func (r *EmployeeDashRpcHandlers) LoginGitHubUser(ctx context.Context, user *employeePackage.GitHubGrpcUser) (*employeePackage.GrpcResponsePayload, error) {
-	//TODO implement me
-	panic("implement me")
+	var emp models.Employee
+	filter := bson.D{{"email", user.GetEmail()}}
+	err := r.mongo.FindOneEmployee(ctx, filter, &emp)
+	ok := r.onFailure(err, logcodes.ERROR, "Employee not registered", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:110")
+	if !ok {
+		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("Employee not registered: %v", err.Error()))
+	}
+
+	if (emp.SignInType != string(oath.GITHUB) || strconv.Itoa(int(user.GetId())) != emp.ProviderId) && emp.SignInType != string(oath.UNKNOWN) {
+		err := errors.New("Invalid credentials")
+		r.onFailure(err, logcodes.ERROR, "Invalid Google credentials", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:117")
+		return nil, status.Errorf(codes.PermissionDenied, err.Error())
+	}
+
+	if emp.SignInType == string(oath.GOOGLE) || emp.SignInType == string(oath.LOCAL) {
+		err := errors.New("Not Authorized")
+		r.onFailure(err, logcodes.ERROR, "Not Authorized", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:123")
+		return nil, status.Errorf(codes.PermissionDenied, err.Error())
+	}
+
+	if emp.SignInType == string(oath.UNKNOWN) {
+		rs, customError := GrpcClient().LoginGitHubUser(ctx, (*userPackage.GitHubGrpcUser)(user))
+		if customError != nil {
+			err := errors.New(customError.Message[0])
+			r.onFailure(err, logcodes.ERROR, customError.Message[0], "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:131")
+			return nil, status.Errorf(codes.Unknown, customError.Message[0])
+		}
+
+		response := employeePackage.GrpcResponsePayload{
+			Status: rs.Status,
+			Jwt:    rs.Jwt,
+			Data:   (*employeePackage.GrpcUser)(rs.Data),
+		}
+		return &response, nil
+	}
+
+	// Encode the JWT
+	jwt, err := utils.EncodeJwt(&emp)
+	ok = r.onFailure(err, logcodes.ERROR, "Unable to encode JWT", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:145")
+	if !ok {
+		return nil, status.Errorf(codes.Internal, fmt.Sprintf("Unable to encode JWT payload"))
+	}
+
+	// Create the gRPC user
+	grpcUser := employeePackage.GrpcUser{
+		Id:                 emp.Id.Hex(),
+		FirstName:          emp.FirstName,
+		LastName:           emp.LastName,
+		Gender:             emp.Gender,
+		Race:               emp.Race,
+		Position:           emp.Position,
+		StartDate:          emp.StartDate.String(),
+		ShiftPreference:    emp.ShiftPreference,
+		BranchName:         emp.BranchName,
+		Region:             emp.Region,
+		Country:            emp.Country,
+		PhoneNumber:        emp.PhoneNumber,
+		Email:              emp.Email,
+		Version:            int32(emp.Version),
+		SignInType:         emp.SignInType,
+		ProviderId:         emp.ProviderId,
+		Password:           emp.Password,
+		UserRole:           emp.UserRole,
+		RegisteredEmployee: emp.Region,
+		Division:           emp.Division,
+	}
+	response := &employeePackage.GrpcResponsePayload{
+		Status: 200,
+		Jwt:    jwt,
+		Data:   &grpcUser,
+	}
+	return response, nil
 }
 
 func (r *EmployeeDashRpcHandlers) LoginLocalUser(ctx context.Context, user *employeePackage.LocalGrpcUser) (*employeePackage.GrpcResponsePayload, error) {
-	//TODO implement me
-	panic("implement me")
+	// TODO: START HERE
+	var emp models.Employee
+	filter := bson.D{{"email", user.GetEmail()}}
+	err := r.mongo.FindOneEmployee(ctx, filter, &emp)
+	ok := r.onFailure(err, logcodes.ERROR, "Employee not registered", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:110")
+	if !ok {
+		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("Employee not registered: %v", err.Error()))
+	}
+
+	if (emp.SignInType != string(oath.GITHUB) || emp.SignInType != string(oath.GOOGLE)) && emp.SignInType != string(oath.UNKNOWN) {
+		err := errors.New("Invalid credentials")
+		r.onFailure(err, logcodes.ERROR, "Invalid Google credentials", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:117")
+		return nil, status.Errorf(codes.PermissionDenied, err.Error())
+	}
+
+	if emp.SignInType == string(oath.UNKNOWN) {
+		rs, customError := GrpcClient().LoginLocalUser(ctx, (*userPackage.LocalGrpcUser)(user))
+		if customError != nil {
+			err := errors.New(customError.Message[0])
+			r.onFailure(err, logcodes.ERROR, customError.Message[0], "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:131")
+			return nil, status.Errorf(codes.Unknown, customError.Message[0])
+		}
+
+		response := employeePackage.GrpcResponsePayload{
+			Status: rs.Status,
+			Jwt:    rs.Jwt,
+			Data:   (*employeePackage.GrpcUser)(rs.Data),
+		}
+		return &response, nil
+	}
+
+	// Encode the JWT
+	jwt, err := utils.EncodeJwt(&emp)
+	ok = r.onFailure(err, logcodes.ERROR, "Unable to encode JWT", "employee-dash/employee-dash-service/services/grpc/employee-grpc-handlers.go:145")
+	if !ok {
+		return nil, status.Errorf(codes.Internal, fmt.Sprintf("Unable to encode JWT payload"))
+	}
+
+	// Create the gRPC user
+	grpcUser := employeePackage.GrpcUser{
+		Id:                 emp.Id.Hex(),
+		FirstName:          emp.FirstName,
+		LastName:           emp.LastName,
+		Gender:             emp.Gender,
+		Race:               emp.Race,
+		Position:           emp.Position,
+		StartDate:          emp.StartDate.String(),
+		ShiftPreference:    emp.ShiftPreference,
+		BranchName:         emp.BranchName,
+		Region:             emp.Region,
+		Country:            emp.Country,
+		PhoneNumber:        emp.PhoneNumber,
+		Email:              emp.Email,
+		Version:            int32(emp.Version),
+		SignInType:         emp.SignInType,
+		ProviderId:         emp.ProviderId,
+		Password:           emp.Password,
+		UserRole:           emp.UserRole,
+		RegisteredEmployee: emp.Region,
+		Division:           emp.Division,
+	}
+	response := &employeePackage.GrpcResponsePayload{
+		Status: 200,
+		Jwt:    jwt,
+		Data:   &grpcUser,
+	}
+	return response, nil
 }
 
 func (r *EmployeeDashRpcHandlers) onFailure(err error, logCode logcodes.LogCodes, message string, origin string) bool {
